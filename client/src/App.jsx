222ef3b60c90c73f632f95321a910a633e8002b6
Nay Htet Aung <nayhtetaung201733@gmail.com>
2022-10-12 20:36:40 +0630

    setup frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,16 @@ import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
+import CssBaseline from '@mui/material/CssBaseline'
+import { ThemeProvider,createTheme } from '@mui/material/styles'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import AppLayout from './components/layout/AppLayout'
+import AuthLayout from './components/layout/AuthLayout'
+import Home from './pages/Home'
+import Signup from './pages/Signup'
+import Board from './pages/Board'
+import Login from './pages/Login'
+
 function App() {
 
   return (
